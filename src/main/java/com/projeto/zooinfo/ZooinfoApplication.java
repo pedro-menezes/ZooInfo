71b1b66b1662d47406fdd48383447d5ab55635a2
pedro-menezes <menezes.pedro10@hotmail.com>
2021-01-28 16:52:46 -0300

    commit inicial

---
+++ src/main/java/com/projeto/zooinfo/ZooinfoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.zooinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/zooinfo/ZooinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.zooinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
