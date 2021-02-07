27f0d81dc7b0172c2d8941cb8a22a3ef644d5dce
Matheus Rocha <matheus.pimenta1@estudante.ufla.br>
2021-02-07 19:24:25 -0300

    Package Model com classes e Pachage Controller iniciado com Interface e classe AnimalControle

---
+++ src/main/java/com/projeto/zooinfo/models/Usuario.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projeto.zooinfo.models;
+
+/**
+ *
+ * @author mathe
+ */
+public class Usuario {
+    private String user;
+    private String senha;
+
+    public Usuario(String user, String senha) {
+        this.user = user;
+        this.senha = senha;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
